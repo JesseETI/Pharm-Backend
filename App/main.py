342f6bf9d64e95db29b5dab8b1ca64fbe8a8ddd4
JesseETI <jesseemamalie2@gmail.com>
2021-04-16 22:22:27 -0400

    DB url works here

--- App/main.py
+++ App/main.py
@@ -47,37 +47,8 @@ storage = firebase.storage()
 
 def create_app():
     app = Flask(__name__, static_url_path='')
-    cors = CORS(app, resources={r"/*": {"origins": "*"}}) 
-    app.config['SQLALCHEMY_DATABASE_URI'] =  CONFIG['dbtype'] + CONFIG['dbuser']+':'+CONFIG['dbpassword']+'@'+CONFIG['dbhost']+  ':' + CONFIG['dbport'] + '/' + CONFIG['dbname']
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.config['SECRET_KEY'] = CONFIG['secret_key']
-    app.config['UPLOADED_PHOTOS_DEST'] = CONFIG['uploadDir']
-    app.config['JWT_EXPIRATION_DELTA'] = timedelta(days=CONFIG['JWTdeltaDays'])
-    app.config['PREFERRED_URL_SCHEME'] = 'https'
-    app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
-    #photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
-    #configure_uploads(app, photos)
-    db.init_app(app)
-    return app
-
-app = create_app()
-
-app.app_context().push()
-
-app.register_blueprint(api_views)
-app.register_blueprint(product_views)
-app.register_blueprint(auth_views)
-app.register_blueprint(search_view)
-app.register_blueprint(order_views)
-app.register_blueprint(customer_views)
-
-jwt = JWT(app, authenticate, identity) 
-
-if __name__ == '__main__':
-    print('Application running in '+CONFIG['ENV']+' mode')
-    app.run(host='0.0.0.0', port=8080, debug=CONFIG['debug'])
-
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://m0k7jdg650wjsxom:scq41zzt7ww7dwpf@grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vsqto06i0kdvymfw'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.config['SECRET_KEY'] = CONFIG['secret_key']
